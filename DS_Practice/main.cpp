e1c58ac3796febd1ab9ad9429b4c9954c53875c3
Amal Eldick <aeldick@amals-macbook-air.local>
2018-09-07 13:08:32 -0700

    Initial Commit

---
+++ DS_Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_Practice
+//
+//  Created by Amal Eldick on 9/7/18.
+//  Copyright © 2018 Amal Eldick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
